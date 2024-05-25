1e1adc3456d98faafed940cd5baf654ff8587701
Siddharthmishraweb <mishrasiddharth1999@gmail.com>
2024-05-25 19:53:30 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import cluster from 'cluster';
 import os from 'os';
 import app from './app';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const numCPUs = os.cpus().length;
 
 if (cluster.isMaster) {
